20ef98fb801742136443b4edffd7979cc8e4b837
Takis <takis.panagiot1@gmail.com>
2020-12-26 21:38:56 +0200

    changed sign in activity

--- app/src/main/java/com/example/eticket/MainActivity.java
+++ app/src/main/java/com/example/eticket/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.eticket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
@@ -29,14 +30,20 @@ public class MainActivity extends AppCompatActivity {
         btnSignIn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-
             }
         });
 
         btnSignUp.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
+            }
+        });
 
+        btnSignIn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent signIn = new Intent(MainActivity.this, Signin.class);
+                startActivity(signIn);
             }
         });
     }
--- app/src/main/java/com/example/eticket/Signin.java
+++ app/src/main/java/com/example/eticket/Signin.java
@@ -3,73 +3,116 @@ package com.example.eticket;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.content.Intent;
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
 
-import com.example.eticket.Model.User;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.rengwuxian.materialedittext.MaterialEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class Signin extends AppCompatActivity {
-    EditText edtPhone,edtPassword;
-    Button btnSignIn;
+    private EditText emailTextView, passwordTextView;
+    private Button Btn;
+    private ProgressBar progressbar;
 
+    private FirebaseAuth mAuth;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        // taking instance of FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
 
-        edtPassword = (MaterialEditText) findViewById(R.id.edtPassword);
-        edtPhone = (MaterialEditText) findViewById(R.id.edtPhone);
-        btnSignIn = (Button) findViewById(R.id.btnSignIn);
+        // initialising all views through id defined above
+        emailTextView = findViewById(R.id.email);
+        passwordTextView = findViewById(R.id.password);
+        Btn = findViewById(R.id.login);
+        progressbar = findViewById(R.id.progressBar);
 
-        //Init firebase
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference table_user = database.getReference("User");
-
-        btnSignIn.setOnClickListener(new View.OnClickListener(){
+        // Set on Click Listener on Sign-in button
+        Btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
-                ProgressDialog mDialog = new ProgressDialog(Signin.this);
-                mDialog.setMessage("Please waiting..");
-                mDialog.show();
-                table_user.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                        //Check if user doesn't exist in database
-                        if(DataSnapshot.child(edtPhone.getText().toString()).exists()){
-                            //Get User information
-                            mDialog.dismiss();
-                            User user = DataSnapshot.child(edtPhone.getText().toString()).getValue(User.class);
-                            if(user.getPassword().equals(edtPassword.getText().toString())){
-                                Toast.makeText(Signin.this, "Sign in successfull",Toast.LENGTH_SHORT).show();
-                            }
-                            else
-                            {
-                                Toast.makeText(Signin.this, "Sign in failed",Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                        else{
-                            Toast.makeText(Signin.this, "User doesn't exist in database!", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
-                    }
-                });
+            public void onClick(View v)
+            {
+                loginUserAccount();
             }
         });
+    }
+
+    private void loginUserAccount()
+    {
+
+        // show the visibility of progress bar to show loading
+        progressbar.setVisibility(View.VISIBLE);
+
+        // Take the value of two edit texts in Strings
+        String email, password;
+        email = emailTextView.getText().toString();
+        password = passwordTextView.getText().toString();
+
+        // validations for input email and password
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(),
+                    "Please enter email!",
+                    Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(),
+                    "Please enter password!",
+                    Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        // signin existing user
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                        new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(
+                                    @NonNull Task<AuthResult> task)
+                            {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getApplicationContext(),
+                                            "Login successful!!",
+                                            Toast.LENGTH_LONG)
+                                            .show();
+
+                                    // hide the progress bar
+                                    progressbar.setVisibility(View.GONE);
+
+                                    // if sign-in is successful
+                                    // intent to home activity
+                                    Intent intent
+                                            = new Intent(Signin.this,
+                                            MainActivity.class);
+                                    startActivity(intent);
+                                }
+
+                                else {
+
+                                    // sign-in failed
+                                    Toast.makeText(getApplicationContext(),
+                                            "Login failed!!",
+                                            Toast.LENGTH_LONG)
+                                            .show();
+
+                                    // hide the progress bar
+                                    progressbar.setVisibility(View.GONE);
+                                }
+                            }
+                        });
     }
 }
